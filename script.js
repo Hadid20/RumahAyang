e66b07c3192ea188007265d5af378a2506910821
Hadid20 <gustihadid56@gmail.com>
2023-02-15 14:19:38 +0700

    RumahAyang

---
+++ script.js
@@ -0,0 +1,10 @@
+var tombolmenu = document.getElementsByClassName('tombol-menu')[0];
+var menu = document.getElementsByClassName('menu')[0];
+
+tombolmenu.Onclick = function() {
+    menu.classList.toggle('active');
+}
+
+menu.Onclick = function() {
+    menu.classList.toggle('active');
+}
