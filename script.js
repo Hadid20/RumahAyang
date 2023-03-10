37b6040a48a97279d5500c35141cd5087533f1f7
Hadid20 <gustihadid56@gmail.com>
2023-03-10 16:16:03 +0700

    partb2

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
-var tombolmenu = document.getElementsByClassName('tombol-menu')[0];
-var menu = document.getElementsByClassName('menu')[0];
+// var tombolmenu = document.getElementsByClassName('tombol-menu')[0];
+// var menu = document.getElementsByClassName('menu')[0];
 
-tombolmenu.Onclick = function() {
-    menu.classList.toggle('active');
-}
+// tombolmenu.Onclick = function() {
+//     menu.classList.toggle('active');
+// }
 
-menu.Onclick = function() {
-    menu.classList.toggle('active');
-}
+// menu.Onclick = function() {
+//     menu.classList.toggle('active');
+// }
+
+function myFunction() {
+    var x = document.getElementById("menu");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
